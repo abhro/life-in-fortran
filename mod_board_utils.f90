8d03afb669540f439ee6281ef5582bd01eecf660
abhro <5664668+abhro@users.noreply.github.com>
2024-03-01 15:05:56 -0500

    Add parameter types to board_utils

--- mod_board_utils.f90
+++ mod_board_utils.f90
@@ -4,14 +4,20 @@ implicit none
 contains
 
   logical function is_corner_cell(board, row, col) ! stub
+    logical, intent(in) :: board(:,:)
+    integer, intent(in) :: row, col
     is_corner_cell = .false.
   end function is_corner_cell
 
   logical function is_edge_cell(board, row, col) ! stub
+    logical, intent(in) :: board(:,:)
+    integer, intent(in) :: row, col
     is_edge_cell = .false.
   end function is_edge_cell
 
   logical function is_center_cell(board, row, col) ! stub
+    logical, intent(in) :: board(:,:)
+    integer, intent(in) :: row, col
     is_center_cell = .false.
   end function is_center_cell
 
